a7d3332ae37a8f1f4162a5e0ae31c037a7b3c275
leidong <leidong@localhost>
2015-04-08 18:37:52 +0800

    add object auth_login

---
+++ auth_login/common/__init__.py
@@ -0,0 +1,21 @@
+# author leidong
+
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+
+
+from  config import ConfigException
+from  config import Config
+CONF = Config()
+
+# --- function -----#
+
+get_subnet = CONF.get_one_section('subnets','net')
+get_passwd = CONF.get_one_section('passwords','user_passwd')
+get_username = CONF.et_one_section('usernames','user_name')
+
+
+__all__ = []
